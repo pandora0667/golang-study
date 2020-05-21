f4ac08b554051edfa39a0d43d1913202ee50020a
seongwon <seongwon@edu.hanbat.ac.kr>
2020-05-21 16:17:23 +0900

    go pool

---
+++ chapter-06/worker-pool.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	f "fmt"
+	"time"
+)
+
+func main() {
+
+	f.Println("worker pool")
+
+	jobs := make(chan int, 100)
+	results := make(chan int, 100)
+
+	for w := 1; w <= 3; w++ {
+		go Worker(w, jobs, results)
+	}
+
+	for j := 1; j <= 5; j++ {
+		jobs <- j
+	}
+	close(jobs)
+
+	for a := 0; a <= 5; a++ {
+		<-results
+	}
+
+}
+
+func Worker(id int, jobs <-chan int, results chan<- int) {
+
+	for j := range jobs {
+		f.Println("worker", id, "started job", j)
+		time.Sleep(time.Second)
+		f.Println("worker", id, "finished", j)
+		results <- j * 2
+	}
+
+}
